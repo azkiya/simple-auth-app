4e071a11da58a5079ca562d585d88388051cca89
fany <isthofany.dev@gmail.com>
2024-07-25 14:03:44 +0700

    add auth sanctum

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('nip')->unique();
             $table->string('nama');
             $table->string('jabatan');
